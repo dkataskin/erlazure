7e7ec5c98e72be525094a88c3c866f3596817bac
Lars Bünting <lars.buenting@smacc.io>
2021-05-27 20:00:38 +0200

    check OTP release

--- src/erlazure.erl
+++ src/erlazure.erl
@@ -777,17 +777,18 @@ get_headers_string(Service, Headers) ->
 
 -spec sign_string(base64:ascii_string(), string()) -> binary().
 -ifdef(OTP_RELEASE).
--if(OTP_RELEASE >= 23).
-sign_string(Key, StringToSign) ->
-  crypto:mac(hmac, sha256, base64:decode(Key), StringToSign).
+  -if(?OTP_RELEASE >= 23).
+    sign_string(Key, StringToSign) ->
+      crypto:mac(hmac, sha256, base64:decode(Key), StringToSign).
+  -else.
+    sign_string(Key, StringToSign) ->
+      crypto:hmac(sha256, base64:decode(Key), StringToSign).
+  -endif.
 -else.
-sign_string(Key, StringToSign) ->
-  crypto:hmac(sha256, base64:decode(Key), StringToSign).
+  sign_string(Key, StringToSign) ->
+    crypto:hmac(sha256, base64:decode(Key), StringToSign).
 -endif.
--else.
-sign_string(Key, StringToSign) ->
-  crypto:hmac(sha256, base64:decode(Key), StringToSign).
--endif.
+
 
 build_uri_base(Service, Account) ->
         lists:concat(["https://", get_host(Service, Account), "/"]).
